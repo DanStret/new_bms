4f9189da59c566bbf69d5fe1d5d8d443fff32926
ander <ander4c9pi@gmail.com>
2024-12-03 09:33:23 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SystemsView from "./views/SystemsView";
+// App.jsx
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import AdminLayout from "./layouts/Admin/Admin.jsx";
+
+import "./assets/scss/black-dashboard-react.scss";
+import "./assets/demo/demo.css";
+import "./assets/css/nucleo-icons.css";
+
+import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper.jsx";
+import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper.jsx";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/admin" element={<SystemsView />} />
-                {/* Otras rutas */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <ThemeContextWrapper>
+      <BackgroundColorWrapper>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/admin/*" element={<AdminLayout />} />
+            <Route
+              path="*"
+              element={<Navigate to="/admin/dashboard" replace />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </BackgroundColorWrapper>
+    </ThemeContextWrapper>
+  );
 }
 
 export default App;
